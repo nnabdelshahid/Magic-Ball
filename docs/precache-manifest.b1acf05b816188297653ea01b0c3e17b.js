cddc3ce82d6436d038304252f920a583155efad9
Nader Abdelshahid <47378552+nnabdelshahid@users.noreply.github.com>
2026-06-28 17:35:16 -0700

    Publish app build for GitHub Pages

---
+++ docs/precache-manifest.b1acf05b816188297653ea01b0c3e17b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb200913481c5bc0141698a485d8490",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e475bd7ea39f956fb8c1",
+    "url": "./static/css/main.9def3714.chunk.css"
+  },
+  {
+    "revision": "0e0e97437240331674bb",
+    "url": "./static/js/2.1986ee63.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1986ee63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e475bd7ea39f956fb8c1",
+    "url": "./static/js/main.4a893e18.chunk.js"
+  },
+  {
+    "revision": "d1134cf4337d6d71f37a",
+    "url": "./static/js/runtime-main.d09b7b37.js"
+  }
+]);
